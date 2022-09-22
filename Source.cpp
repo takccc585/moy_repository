7939817fe4f6932e6b51bdcc01bd32a9fdd3aadd
takccc585 <114169693+takccc585@users.noreply.github.com>
2022-09-22 18:04:49 +0300

    classnya rabota 22.09.2022

---
+++ Source.cpp
@@ -0,0 +1,124 @@
+#include<iostream>
+using namespace std;
+int main() {
+	//int a;
+	//cin >> a;
+	//float b = a / 100;
+	//cout << b;
+
+	//int a;
+	//cin >> a;
+	//float b = a / 1000;
+	//cout << b;
+
+	//int a;
+	//cin >> a;
+	//float b = a / 1024;
+	//cout << b;
+
+	//int a,c;
+	//cin >> a >> c;
+	//float b = a / c;
+	//cout << b;
+
+	//int a , c;
+	//cin >> a >> c;
+	//float b = a % c;
+	//cout << b;
+
+	//int a;
+	//cin >> a ;
+	//float b1 = a % 10;
+	//float b = a / 10;
+	//cout << b << " " << b1;
+
+	/*int a;
+	cin >> a;
+	float b1 = a % 10;
+	float b = a / 10;
+	int c = b1 + b;
+	int c1 = b1 * b;
+	cout << c << " " << c1;*/
+
+	/*int a;
+	cin >> a ;
+	float b = a % 10;
+	float b1 = a / 10;
+	int c = 10*b + b1;
+	cout << c;*/
+
+	/*int a;
+	cin >> a;
+	float b1 = a / 100;
+	cout << b1;*/
+
+	/*int a;
+	cin >> a;
+	float  bb = a / 10;
+	float  bbb = a % 10;
+	int c = 100 * bbb + bb;
+	cout << c;*/
+
+	/*int a;
+	cin >> a;
+	float b = a / 100;
+	float  bb = (a/10) % 10;
+	float  bbb = (a/10) % 10;
+	int c = b + bb + bbb;
+	int cc = b * bb * bbb;
+	cout << c << " " << cc;*/
+
+	/*int a;
+	cin >> a;
+	float b = a / 100;
+	float  bb = (a/10) % 10;
+	float  bbb = a % 10;
+	int c = 100 * bbb + 10 * bb + b;
+	cout << c ;*/
+
+	/*int a;
+	cin >> a;
+	float b = a / 100;
+	float  bb = (a / 10) % 10;
+	float  bbb = a % 10;
+	int c = 100 * bb + 10 * bbb + b;
+	cout << c;*/
+
+	/*int a;
+	cin >> a;
+	float b = a / 100;
+	float  bb = (a / 10) % 10;
+	float  bbb = a % 10;
+	int c = 100 * bbb + 10 * b + bb;
+	cout << c;*/
+
+	/*int a;
+	cin >> a;
+	float b = a / 100;
+	float  bb = (a / 10) % 10;
+	float  bbb = a % 10;
+	int c = 100 * bb + 10 * b + bbb;
+	cout << c;*/
+
+	/*int a;
+	cin >> a;
+	float b = (a / 100) % 10;
+	cout << b;*/
+
+	/*int a;
+	cin >> a;
+	float b = a / 1000;
+	cout << b;*/
+
+	/*int a;
+	cin >> a;
+	float b = a / 60;
+	cout << b;*/
+
+	int a;
+	cin >> a;
+	float b = a /3600;
+	cout << b;
+
+	return 0;
+}
